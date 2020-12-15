cddfd2831bd62de56a182cd10b21b5b211e9b72b
Joseph Williams <josephwilliamsjs@gmail.com>
2020-12-15 23:08:36 +0000

    incorporated momentjs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,7 @@
 
  // Test / driver code (temporary). Eventually will get this from the server.
  // Fake data taken from initial-tweets.json
+
 const data = [
   {
     "user": {
@@ -59,7 +60,7 @@ $( document ).ready(function() {
       </div>
   
       <footer>
-        <p>${tweetObj.created_at}</p>
+        <p>${moment(tweetObj.created_at).fromNow()}</p>
         <p class="icons"><i class="fas fa-flag"></i> <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></p>
       </footer>
       </article>
