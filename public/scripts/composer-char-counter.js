2aaced49f74955619b77cc3a8f36e3aa34bea950
Joseph Williams <josephwilliamsjs@gmail.com>
2020-12-15 01:18:03 +0000

    feat: notify user how many characters they have left in their tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $('#tweet-text').on('input', function() {
+    const charLimit = 140;
+    let charsLeft = charLimit - $(this).val().length
+    const counter= $(this).parent().children('.tweet-button').children('.counter');
+    // update counter value
+    counter.val(charsLeft); 
+    if (charsLeft <= 0) {
+      counter.addClass('characters-exceeded');
+    } else {
+      counter.removeClass('characters-exceeded');
+    }
+  })
+});
