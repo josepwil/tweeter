515eee9184ce17efa4665b2b36388967f930dcd7
Joseph Williams <josephwilliamsjs@gmail.com>
2020-12-16 22:16:06 +0000

    feat: fetching tweets from server with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,8 @@
  */
  // Test / driver code (temporary). Eventually will get this from the server.
  // Fake data taken from initial-tweets.json
- const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
-$( document ).ready(function() {
+$(document).ready(function() {
   const createTweetElement = function(tweetObj) {
     const $markup = $(`
       <article class="tweet">
@@ -64,9 +40,7 @@ $( document ).ready(function() {
     }
   }
 
-  renderTweets(data);
-
-  // ajax form submission
+  // ajax post form submission
   $('form').on('submit', function(event) {
     event.preventDefault();
     $.ajax({ method: 'POST', url: '/tweets', data: $(this).serialize() })
@@ -75,7 +49,14 @@ $( document ).ready(function() {
     })
   })
 
-
-
+  // ajax get 
+  const loadTweets = function() {
+    $.ajax({method: 'GET', url: '/tweets', dataType: 'JSON'})
+    .done(function(response) {
+      console.log(response);
+      renderTweets(response);
+    });
+  }
+  loadTweets();
 });
 
